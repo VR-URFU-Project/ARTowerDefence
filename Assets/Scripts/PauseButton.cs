3eb98f8dc47a714a7d3d49e48e57722cb6169b9f
Etien <stepan.filippov@yandex.ru>
2022-10-31 20:13:45 +0500

    Добавлена кнопка паузы

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour
+{
+    [SerializeField] Button pauseButton;
+    [SerializeField] Button startButton;
+    private int flag = 0;
+
+    private void Start()
+    {
+        pauseButton.GetComponentInChildren<Text>().text = (flag == 0) ? "Pause" : "Resume";
+    }
+
+    /// <summary>
+    /// При нажатии на кнопку
+    /// </summary>
+    public void PressButton()
+    {
+        Time.timeScale = flag;
+        if(flag == 0)
+        {
+            flag = 1;
+            pauseButton.GetComponentInChildren<Text>().text = "Resume";
+            startButton.gameObject.SetActive(false);
+        }
+        else
+        {
+            flag = 0;
+            pauseButton.GetComponentInChildren<Text>().text = "Pause";
+            startButton.gameObject.SetActive(true);
+        }
+    }
+}

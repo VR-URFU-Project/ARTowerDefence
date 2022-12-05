eee8c7fe434b405ac72a8a4fcaf521778f0307ad
AnesVijay <2011andreyka@gmail.com>
2022-12-05 18:06:57 +0500

    Adjusted size and switching holograms
    + different Plane for Windows version

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -19,6 +19,17 @@ public class Shape : MonoBehaviour
 
     private GameObject mainCanvas;
 
+
+    [Header("Materials")]
+    [SerializeField] private Material GreenMatPlane;
+    [SerializeField] private Material GreenMatSphere;
+
+    [SerializeField] private Material RedMatPlane;
+    [SerializeField] private Material RedMatSphere;
+
+    [SerializeField] private GameObject MaterialToChange_plane;
+    [SerializeField] private GameObject MaterialToChange_sphere;
+
     void Start()
     {
         shop = GameObject.FindGameObjectWithTag("Shop").GetComponent<Shop>();
@@ -28,6 +39,17 @@ public class Shape : MonoBehaviour
         gamingPlace = GameObject.FindWithTag("GamingPlace");
         YesNoPanel.SetActive(false);
         mainCanvas.SetActive(false);
+
+        //MaterialToChange_plane = gameObject.GetComponent<Renderer>().material;
+        //MaterialToChange_sphere = gameObject.GetComponent<Renderer>().material;
+
+        //this_mat_plane = gameObject.FindC<Renderer>().material;
+        //this_mat_sphere = gameObject.GetComponentInChildren<Renderer>().material;
+
+        //Green = new Color(0, 255, 20, 0.5f);
+        //Red = new Color(255, 3, 0, 0.5f);
+        //Green.r = 0f; Green.g = 255f; Green.b = 20f; Green.a = 0;
+        //Red.r = 255f; Red.g = 2f; Red.b = 0f; Red.a = 0;
     }
 
     void Update()
@@ -35,8 +57,24 @@ public class Shape : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (!mouseButtonUped && Physics.Raycast(ray, out hit))
         {
+            MaterialToChange_plane.GetComponent<Renderer>().material = GreenMatPlane;
+            MaterialToChange_sphere.GetComponent<Renderer>().material = GreenMatSphere;
             if (hit.collider.gameObject.name == "Plane")
                 transform.position = hit.point;
+            else
+            {
+                transform.position = hit.point;
+                MaterialToChange_plane.GetComponent<Renderer>().material = RedMatPlane;
+                MaterialToChange_sphere.GetComponent<Renderer>().material = RedMatSphere;
+            }
+        }
+        else
+        {
+            if (!mouseButtonUped)
+            {
+                MaterialToChange_plane.GetComponent<Renderer>().material = RedMatPlane;
+                MaterialToChange_sphere.GetComponent<Renderer>().material = RedMatSphere;
+            }
         }
 
         if (Input.GetMouseButtonUp(0))

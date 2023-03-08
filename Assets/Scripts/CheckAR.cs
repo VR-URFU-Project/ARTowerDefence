8dd3ec5ab117e8984cb964dce60019f2742e6248
AnesVijay <2011andreyka@gmail.com>
2023-03-08 23:19:35 +0500

    Graphics update + CheckAR script in files

--- Assets/Scripts/CheckAR.cs
+++ Assets/Scripts/CheckAR.cs
@@ -7,20 +7,30 @@ public class CheckAR : MonoBehaviour
     [SerializeField] private Button AR_button;
     [SerializeField] private GameObject text;
 
+
     private void Awake()
     {
+        DoCheck();
+    }
+
+    private void DoCheck()
+    {
+        bool textCanBeActive = false;
+
         ARSession.CheckAvailability();
 
         if (ARSession.state == ARSessionState.Unsupported)
         {
-            if (text != null)
-                text.SetActive(true);
-
             if (AR_button != null)
+                textCanBeActive = true;
                 AR_button.GetComponent<Button>().interactable = false;
         }
         else
             if (AR_button != null)
-                AR_button.GetComponent<Button>().interactable = true;
+            AR_button.GetComponent<Button>().interactable = true;
+
+        if (textCanBeActive)
+            if (text != null)
+                text.SetActive(true);
     }
 }

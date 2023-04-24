a9a91d2f493c8a4078f6855351d23280655bd6bb
AnesVijay <2011andreyka@gmail.com>
2023-04-24 12:10:56 +0500

    1. reapaired ranges of all towers 2. added new range for Ballista 3. repaired CheckAR script 4. added new text to LocalizationManager

--- Assets/Scripts/UIScripts/CheckAR.cs
+++ Assets/Scripts/UIScripts/CheckAR.cs
@@ -14,22 +14,25 @@ public class CheckAR : MonoBehaviour
 
     private void DoCheck()
     {
-        bool textCanBeActive = false;
+        text.SetActive(false);
 
-        ARSession.CheckAvailability();
-
-        if (ARSession.state == ARSessionState.Unsupported)
-        {
-            if (AR_button != null)
-                textCanBeActive = true;
+        try {
+            ARSession.CheckAvailability();
+            if (ARSession.state == ARSessionState.Unsupported || ARSession.state == ARSessionState.None)
+            {
                 AR_button.GetComponent<Button>().interactable = false;
-        }
-        else
-            if (AR_button != null)
-            AR_button.GetComponent<Button>().interactable = true;
-
-        if (textCanBeActive)
-            if (text != null)
                 text.SetActive(true);
+            }
+            else
+                AR_button.GetComponent<Button>().interactable = true;
+                    
+        } 
+        catch{
+            text.SetActive(false);
+            AR_button.GetComponent<Button>().interactable = false;
+        }
+        
+
+        
     }
 }

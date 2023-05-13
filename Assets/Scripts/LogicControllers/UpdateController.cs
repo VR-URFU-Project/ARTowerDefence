5b91265a680ed461428f0b9f779ac9cba18b2422
Etien <stepan.filippov@yandex.ru>
2023-05-14 03:10:30 +0500

    restricted updates

--- Assets/Scripts/LogicControllers/UpdateController.cs
+++ Assets/Scripts/LogicControllers/UpdateController.cs
@@ -8,8 +8,15 @@ public static class UpdateController
 
     public static string GetUpdateString(TowerType towerType, int currentLevel)
     {
-        if (currentLevel > LevelData[towerType].Count)
+        if (!CanUpgradeTower(towerType, currentLevel))
             return null;
         return LevelData[towerType][currentLevel - 1];
+    }
+
+    public static bool CanUpgradeTower(TowerType towerType, int currentLevel)
+    {
+        if (currentLevel > LevelData[towerType].Count)
+            return false;
+        return true;
     }
 }

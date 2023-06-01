0716a133f932694a27d9fb72684bf3142f26bdab
AnesVijay <2011andreyka@gmail.com>
2023-06-01 11:26:10 +0500

    money to 100

--- Assets/Scripts/LogicControllers/MoneySystem.cs
+++ Assets/Scripts/LogicControllers/MoneySystem.cs
@@ -5,7 +5,7 @@ using CI.QuickSave;
 
 public static class MoneySystem
 {
-    private const int DEFAULT_MONEY = 500;
+    private const int DEFAULT_MONEY = 100;
     private static int money = DEFAULT_MONEY;
 
     /// <summary>

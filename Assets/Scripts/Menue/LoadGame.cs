503f8aa0e51dd2fd61bb7da418bf3f3e3bedcb27
AnesVijay <2011andreyka@gmail.com>
2023-06-12 17:19:22 +0500

    changed loadGame logic

--- Assets/Scripts/Menue/LoadGame.cs
+++ Assets/Scripts/Menue/LoadGame.cs
@@ -8,22 +8,22 @@ public class LoadGame : MonoBehaviour
 {
     public void LoadSavedGame()
     {
-        //var reader = QSReader.Create("SceneData");
-        //if(!reader.Exists("scene"))
-        //{
-        //    return;
-        //}
-        //var index = reader.Read<int>("scene");
+        var reader = QSReader.Create("SceneData");
+        if (!reader.Exists("scene"))
+        {
+            return;
+        }
+        var index = reader.Read<int>("scene");
 
         var writer = QuickSaveWriter.Create("Temp");
-        var reader = QSReader.Create("DataExists");
+        reader = QSReader.Create("DataExists");
         if (reader.Exists("HasSavedData") && reader.Read<int>("HasSavedData") == 1)
             writer.Write("needsLoad", 1);
         else
             writer.Write("needsLoad", 0);
         writer.Commit();
 
-        //SceneManager.LoadScene(index);
+        SceneManager.LoadScene(index);
     }
 
     public void NotLoadSavedGame()
